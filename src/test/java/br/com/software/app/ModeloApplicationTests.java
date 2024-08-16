1088c914339292c7b48bd81a9f9fa783766eaecd
lucianolucas <luciano.lucas@bllc.com.br>
2024-08-15 22:41:57 -0300

    first commit

---
+++ src/test/java/br/com/software/app/ModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.software.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
